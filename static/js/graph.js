409c467f5dbfd2ec0fa8f56bea4f07f6b4fc4c35
woodfordtim <woodfordtim@gmail.com>
2019-10-22 20:47:52 +0000

    data issue

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,7 +1,31 @@
 queue()
-    .defer(d3.csv, "data/assessment.csv")
+    .defer(d3.csv, "data/assessment2.csv")
     .await(makeGraphs);
 
-function makeGraphs(errors, assessmentData) {
+function makeGraphs(error, test) {
+    var ndx = crossfilter(test)
 
+    classData(ndx);
+
+    dc.renderAll();
+}
+
+function classData(ndx) {
+    var dim = ndx.dimension(dc.pluck('standard'));
+    console.log("data")
+    console.log(dim)
+    var group = dim.group();
+
+    dc.barChart("#class1")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("age")
+        .yAxis().ticks(20);
 }
