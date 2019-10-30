2163b4f15e7ef3781b9b5fee38081c90fba56e35
woodfordtim <woodfordtim@gmail.com>
2019-10-30 12:34:35 +0000

    add all basic subject barcharts for reading, writing and maths

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,22 +1,32 @@
 queue()
-    .defer(d3.csv, "data/assessment2.csv")
+    .defer(d3.csv, "data/assessment3.csv")
     .await(makeGraphs);
 
-function makeGraphs(error, test) {
-    var ndx = crossfilter(test)
+function makeGraphs(error, schoolData) {
+    var ndx = crossfilter(schoolData);
 
-    classData(ndx);
+    cohortSelector(ndx);
+    readingGraph(ndx);
+    writingGraph(ndx);
+    mathematicsGraph(ndx);
 
     dc.renderAll();
 }
 
-function classData(ndx) {
-    var dim = ndx.dimension(dc.pluck('standard'));
-    console.log("data")
-    console.log(dim)
+function cohortSelector(ndx) {
+    dim = ndx.dimension(dc.pluck("Cohort"));
+    group = dim.group();
+
+    dc.selectMenu("#cohortSelector")
+    .dimension(dim)
+    .group(group);
+}
+
+function readingGraph(ndx) {
+    var dim = ndx.dimension(dc.pluck('Reading'));
     var group = dim.group();
 
-    dc.barChart("#class1")
+    dc.barChart("#reading")
         .width(400)
         .height(300)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
@@ -26,6 +36,42 @@ function classData(ndx) {
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("age")
-        .yAxis().ticks(20);
+        .xAxisLabel("standard")
+        .yAxis().ticks(5);
+}
+
+function writingGraph(ndx) {
+    var dim = ndx.dimension(dc.pluck('Writing'));
+    var group = dim.group();
+
+    dc.barChart("#writing")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("standard")
+        .yAxis().ticks(5);
+}
+
+function mathematicsGraph(ndx) {
+    var dim = ndx.dimension(dc.pluck('Mathematics'));
+    var group = dim.group();
+
+    dc.barChart("#maths")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("standard")
+        .yAxis().ticks(5);
 }
