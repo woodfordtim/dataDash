3989d5a100498e58ad3e4354e8d60a9fbb3cd473
woodfordtim <woodfordtim@gmail.com>
2019-11-21 20:35:56 +0000

    trying to add select menus

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,11 +1,12 @@
 queue()
-    .defer(d3.csv, "data/assessment3.csv")
+    .defer(d3.csv, "data/assessment5.csv")
     .await(makeGraphs);
 
 function makeGraphs(error, schoolData) {
     var ndx = crossfilter(schoolData);
 
-    cohortSelector(ndx);
+    subjectSelector1(ndx);
+    subjectSelector2(ndx);
     readingGraph(ndx);
     writingGraph(ndx);
     mathematicsGraph(ndx);
@@ -17,23 +18,38 @@ function makeGraphs(error, schoolData) {
     dc.renderAll();
 }
 
-function cohortSelector(ndx) {
+function subjectSelector1(ndx) {
     dim = ndx.dimension(dc.pluck("Cohort"));
     group = dim.group();
 
-    dc.selectMenu("#cohortSelector")
-    .dimension(dim)
-    .group(group);
-}
+    dc.selectMenu("#subjectSelector1")
+        .dimension(dim)
+        .group(group); 
+} 
+
+function subjectSelector2(ndx) {
+    dim = ndx.dimension(dc.pluck("Cohort"));
+    group = dim.group();
+
+    dc.selectMenu("#subjectSelector2")
+        .dimension(dim)
+        .group(group); 
+} 
 
 function readingGraph(ndx) {
     var dim = ndx.dimension(dc.pluck('Reading'));
     var group = dim.group();
 
+
+    /*how do i get a selectMenu to target this chart??
+        dc.selectMenu("#subjectSelector2")
+        .dimension(dim)
+        .group(group); */
+
     dc.barChart("#reading")
         .width(400)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
@@ -51,7 +67,7 @@ function writingGraph(ndx) {
     dc.barChart("#writing")
         .width(400)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
@@ -69,47 +85,91 @@ function mathematicsGraph(ndx) {
     dc.barChart("#maths")
         .width(400)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("standard")
+        .xAxisLabel("Standard")
         .yAxis().ticks(5);
 }
 
 function testGraph(ndx) {
-    var mathsDimension = ndx.dimension(dc.pluck('Mathematics'));
-    var standard_achieved = mathsDimension.group();
+    var readingDimension = ndx.dimension(dc.pluck("Reading"));
+    var standard_achieved_Skipper = readingDimension.group().reduceSum(function (d) {
+        if (d.Cohort === 'Skipper') {
+            return +d.Reading;
+        } else {
+            return 0;
+        }
+    });
 
     dc.pieChart("#testGraph")
         .width(450)
         .height(300)
-        .slicesCap(4)
         .innerRadius(50)
-        .dimension(mathsDimension)
-        .group(standard_achieved)
+        .dimension(readingDimension)
+        .group(standard_achieved_Skipper)
         .legend(dc.legend())
 }
+
+function testGraph2(ndx) {
+    var readingDimension = ndx.dimension(dc.pluck("Reading"));
+    var standard_achieved_Admiral = readingDimension.group().reduce(
+
+        // Add a Fact
+        function(p, v) {
+            p.count++;
+            p.total += v.Reading;
+            p.average = p.total / p.count;
+            return p;
+        },
+        // Remove a Fact
+        function(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+                p.average = 0;
+            } else {
+                p.total -= v.spend;
+                p.average = p.total / p.count;
+            }
+            return p;
+        },
+        // Initialise the Reducer
+        function () {
+            return { count: 0, total: 0, average: 0 };
+        }
+            
+            
+            );
+
+    dc.pieChart("#testGraph2")
+        .width(450)
+        .height(300)
+        .innerRadius(50)
+        .dimension(readingDimension)
+        .group(standard_achieved_Skipper)
+        .legend(dc.legend())
+    }
 
 function contextGender(ndx) {
     var genderDimension = ndx.dimension(dc.pluck('Gender'));
     var genderComparison = genderDimension.group();
 
     dc.pieChart("#gender")
-        .width(450)
-        .height(300)
-        .slicesCap(4)
-        .innerRadius(50)
+        .height(200)
+        .radius(90)
+        .innerRadius(30)
         .dimension(genderDimension)
         .group(genderComparison)
         .legend(dc.legend())
 }
 
 function contextPupilPremium(ndx) {
-    var pupilPremiumDimension = ndx.dimension(dc.pluck('pupilPrem'));
+    var pupilPremiumDimension = ndx.dimension(dc.pluck('Pupil Premium'));
     var pupilPremiumComparison = pupilPremiumDimension.group();
 
     dc.pieChart("#pupilPremium")
